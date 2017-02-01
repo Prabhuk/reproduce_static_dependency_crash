9eafec13c1d5a97987186d713c287b70c0c3d601
prabhu <rajasekp@uci.edu>
2017-02-01 12:53:35 -0800

    Static lib proj

---
+++ libstatic/cmake-build-debug/CMakeFiles/unuseful.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unuseful.dir/src/Unuseful.cpp.o"
+  "libunuseful.pdb"
+  "libunuseful.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unuseful.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
