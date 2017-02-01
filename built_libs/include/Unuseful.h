8ffa56d81dbabfa68111a2562600443455cf746d
prabhu <rajasekp@uci.edu>
2017-02-01 12:49:57 -0800

    Added first client example

---
+++ built_libs/include/Unuseful.h
@@ -0,0 +1,14 @@
+//
+// Created by Prabhu Rajasekaran on 2/1/17.
+//
+
+#ifndef UNUSEFUL_UNUSEFUL_H
+#define UNUSEFUL_UNUSEFUL_H
+
+class Unuseful {
+public:
+    void printSomething();
+};
+
+
+#endif //UNUSEFUL_UNUSEFUL_H
---
+++ client/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Unuseful.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    Unuseful u;
+    u.printSomething();
+    return 0;
+}
